924d8b078ba45a248c5cb25b54075ce12e5a2118
Semyon <61237950+SemafonKA@users.noreply.github.com>
2022-10-13 15:11:35 +0700

    ФИкс мИНУСА

--- Lab3/Lab3_main.cpp
+++ Lab3/Lab3_main.cpp
@@ -11,18 +11,18 @@ int main() {
    char inpStr[256];
    char outStr[512];
    uint32_t outStrLen = 512;
-   uint32_t repeats = 0;
+   int32_t repeats = 0;
    cout << "Введите строку для дублирования: ";
    cin >> inpStr;
 
    cout << "Введите число повторов: ";
    cin >> repeats;
-   if (repeats == 0) {
+   if (repeats <= 0) {
       cout << "Введено неверное число повторов. Завершение программы." << endl;
       return -1;
    }
 
-   DublStr(inpStr, strlen(inpStr), outStr, &outStrLen, repeats);
+   DublStr(inpStr, strlen(inpStr), outStr, &outStrLen, static_cast<uint32_t>(repeats));
 
    cout << "Полученная строка: " << outStr << endl;
    return 0;
